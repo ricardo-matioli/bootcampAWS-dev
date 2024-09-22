36b936933ce52674f3c6a9055a74c8646d80ae30
ricardo-matioli <ricardomatioli@yahoo.com.br>
2024-09-22 17:47:56 -0300

    AJUSTE GIT IGNORE

--- hop/hop-configs.bat
+++ hop/hop-configs.bat
@@ -1,6 +1,6 @@
 rem rodar no cmd aberto como administrator
-setx HOP_JAVA_HOME "C:\Program Files\Java\jdk-11" /M
-setx HOP_CONFIG_FOLDER "C:\opt\dev-repository\hop\config" /M
-setx HOP_AUDIT_FOLDER "C:\opt\dev-repository\hop\audit" /M
+setx HOP_JAVA_HOME "C:\Program Files\Java\jdk-23" /M
+setx HOP_CONFIG_FOLDER "C:\opt\bootcampAWS-dev\hop\config" /M
+setx HOP_AUDIT_FOLDER "C:\opt\bootcampAWS-dev\hop\audit" /M
 setx HOP_OPTIONS "-Xms512m -Xmx2048m -Dfile.encoding=utf8 -Duser.timezone=UTC" /M
-setx HOP_SHARED_JDBC_FOLDERS "C:\opt\dev-repository\hop\jdbc" /M
+setx HOP_SHARED_JDBC_FOLDERS "C:\opt\bootcampAWS-dev\hop\jdbc" /M
